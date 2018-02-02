b4b9f2b33080e7c73435b695893936262f987392
yangyf <yangyf@zonekey.com.cn>
2018-02-02 17:28:26 +0800

    xx

--- hello_world.py
+++ hello_world.py
@@ -1,32 +1,44 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QDesktopWidget
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QCoreApplication
 
 
+class MyWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-class MyWidge(QWidget):
+    def initUI(self):
+        btn = QPushButton(QIcon("pen.ico"), "zk", self)
+        # 事件绑定
+        # btn.clicked.connect(QCoreApplication.instance().quit)
 
-	def __init__(self):
-		super().__init__()
-		self.initUI()
+        btn.setToolTip("test tooltip")
+        btn.setGeometry(50, 50, 50, 50)
+        # btn.move(50, 50)
+        # 居中
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 
-	def initUI(self):
-		btn = QPushButton(QIcon("pen.ico"), "zk", self)
-		btn.setToolTip("test tooltip")
-		btn.setGeometry(50, 50, 50, 50)
-		#btn.move(50, 50)
+        #self.setGeometry(20, 20, 200, 200)
+        self.setWindowTitle("icon test")
+        self.setWindowIcon(QIcon("pen.ico"))
+        self.show()
 
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, "Message", "要退出吗？", QMessageBox.Yes, QMessageBox.No)
 
-
-		self.setGeometry(20, 20, 200, 200)
-		self.setWindowTitle("icon test")
-		self.setWindowIcon(QIcon("pen.ico"))
-		self.show()
-
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 
 if __name__ == "__main__":
-	app = QApplication(sys.argv)
-	ex = MyWidge()
-	sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    sys.exit(app.exec_())
---
+++ menu_toolbar.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, qApp, QMenu
+from PyQt5.QtGui import QIcon
+
+
+class MyWindows(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        # exitAct = QAction(QIcon("pen.ico"), "&Exit", self)
+        # exitAct.setShortcut("Ctrl+Q")
+        # exitAct.setStatusTip("退出")
+        # exitAct.triggered.connect(qApp.quit)
+        #
+        # menubar = self.menuBar()
+        # menubar.addMenu("&File")
+        # menubar.addAction(exitAct)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+
+        impMenu = QMenu('Import', self)
+        impAct = QAction('Import mail', self)
+        impMenu.addAction(impAct)
+
+        newAct = QAction('New', self)
+
+        fileMenu.addAction(newAct)
+        fileMenu.addMenu(impMenu)
+
+
+        self.statusBar().showMessage("准备..")
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("statusbar")
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    wnd = MyWindows()
+
+    sys.exit(app.exec_())
